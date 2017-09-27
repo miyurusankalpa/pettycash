4ec8bbcd9cd839520d1d3d2c08b8be43d8fad764
it17142038 <32291139+it17142038@users.noreply.github.com>
2017-09-27 08:12:11 +0530

    Update Manager.h

--- pettycash/Manager.h
+++ pettycash/Manager.h
@@ -1,6 +1,18 @@
+// Manager.h
+// 27-09-2017
+/* Name: Manager
+   ===========================================
+   Responsibilities	   |	Collaborations
+   ------------------------+------------------
+   View monthly reports.   |	Account
+   View remaining funds.   |	Staff
+   Change login PIN.	   |			*/
+
 #pragma once
 class Manager
 {
+private:
+	
 public:
 	Manager();
 	~Manager();

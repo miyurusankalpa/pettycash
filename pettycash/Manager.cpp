c014bef63da6846914751e4061de2184dde65120
it17142038 <32291139+it17142038@users.noreply.github.com>
2017-09-27 08:13:27 +0530

    Update Manager.cpp

--- pettycash/Manager.cpp
+++ pettycash/Manager.cpp
@@ -1,3 +1,13 @@
+// Manager.cpp
+// 27-09-2017
+/* Name: Manager
+   ===========================================
+   Responsibilities	   |	Collaborations
+   ------------------------+------------------
+   View monthly reports.   |	Account
+   View remaining funds.   |	Staff
+   Change login PIN.	   |			*/
+
 #include "stdafx.h"
 #include "Manager.h"
 

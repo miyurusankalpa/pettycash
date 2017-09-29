f6a7e6cdcb5b25ba0e954c8f85b9ff50a969adda
it17142038 <32291139+it17142038@users.noreply.github.com>
2017-09-29 12:30:39 +0530

    Update Manager.cpp

--- pettycash/Manager.cpp
+++ pettycash/Manager.cpp
@@ -9,14 +9,23 @@
    Change login PIN.       |                    */
 
 #include "stdafx.h"
+#inlclude <iostream>
 #include "Manager.h"
-
 
 Manager::Manager(int newPIN)
 {
    PIN = newPIN;
 }
 
+Manager::viewMonthlyReport()
+{
+   cout<<"~~~ Monthly Report ~~~\n"<<endl;
+}
+
+Manager::viewRemainingFunds()
+{
+   cout<<"Remaining funds: Rs. "<<account.funds<<endl;
+}
 
 Manager::~Manager()
 {

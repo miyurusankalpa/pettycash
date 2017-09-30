71ac09b7099af672f413b1d903cf9accd69ca74d
pinsara <32256306+pinsara@users.noreply.github.com>
2017-09-30 21:18:48 +0530

    Update Administrator.h

--- pettycash/Administrator.h
+++ pettycash/Administrator.h
@@ -1,8 +1,21 @@
 #pragma once
 class Administrator
 {
+private:
+
+	char Admin_ID[10];
+	char Acc_Pin[10];
+	char User_Name[50];
+	char User_Id[10];
+	char User_Pin[10];
+
 public:
+
 	Administrator();
-	~Administrator();
+	Administrator(char pAdmin_ID[10],char pAcc_Pin[10],char pUser_Name[50],char pUser_Id[10],char pUser_Pin[10]);
+	void SetAccountPin();
+	void AddUser();
+	void RemoveUser();
+	~Administrator(void);
 };
 

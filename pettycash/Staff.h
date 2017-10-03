b625ab5ced3d7854726765e2c8ca64556046d017
KannangaragePU <32173612+KannangaragePU@users.noreply.github.com>
2017-10-04 01:07:49 +0530

    Update Staff.h

--- pettycash/Staff.h
+++ pettycash/Staff.h
@@ -5,9 +5,17 @@
 class Staff : public User
 {
 private:
+	int PIN;
+	string name;
+	float balance;
+	float limits;
 	
 public:
 	Staff();
+	void changePIN(int oldPIN, int newPIN);
+	void setLimit( float limit );
+	void changeBal( float req );
+	void viewBal( void );
 	~Staff();
 };
 

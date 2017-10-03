e2a2aff596c7c22bd27d5a3e7534a27ca8e77cf3
KannangaragePU <32173612+KannangaragePU@users.noreply.github.com>
2017-10-04 01:07:41 +0530

    Update Staff.cpp

--- pettycash/Staff.cpp
+++ pettycash/Staff.cpp
@@ -4,9 +4,29 @@
 
 Staff::Staff()
 {
+  
 }
 
-
+void Staff::changePIN(int oldPIN, int newPIN)
+{
+  PIN = userPIN;
+  name = username;
+} 
+void setLimit( float limit )
+{
+  balance = limit;
+  limits = limit;
+}  
+void changeBal( float req )
+{
+  balance = balance - req
+  cout<<"Request Procceded"<<endl;
+}
+void viewBal( void )
+{
+  cout<<"Limit   : "<< limits<<endl;
+  cout<<"Balance : "<< balance<<endl;
+} 
 Staff::~Staff()
 {
 }

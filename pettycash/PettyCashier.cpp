194cfd2cdf3aa456f9ddc3566d97c87f74648bc9
ugc286 <32336127+ugc286@users.noreply.github.com>
2017-10-04 08:40:41 -0700

    Update PettyCashier.cpp

--- pettycash/PettyCashier.cpp
+++ pettycash/PettyCashier.cpp
@@ -1,11 +1,28 @@
 #include "stdafx.h"
 #include "PettyCashier.h"
+#include <iostream>
 
+using namespace std;
 
-PettyCashier::PettyCashier()
+PettyCashier::PettyCashier(int newPIN)
 {
+  PIN = newPIN;
 }
 
+void pettyCashier::withdrawPettyCash(int amount)
+{
+  accBal = accBal - amount;   
+}
+
+void pettyCashier::retrieveAccReports()
+{
+  cout<<"**Account Usage Report**\n"<<endl;
+}
+
+void pettyCashier::setTransactions()
+{
+  cout<<"**Transactions**\n"<<endl;
+}
 
 PettyCashier::~PettyCashier()
 {

604bd96538345afb5e2729fa2faa22707bc3611f
ugc286 <32336127+ugc286@users.noreply.github.com>
2017-10-04 08:50:37 -0700

    Update PettyCashier.h

--- pettycash/PettyCashier.h
+++ pettycash/PettyCashier.h
@@ -1,8 +1,14 @@
 #pragma once
 class PettyCashier
 {
+private:
+	int PIN;
+	int accBal:
 public:
-	PettyCashier();
+	PettyCashier(int nw_pin);
+	void withdrawPettyCash(int amount);
+	void retrieveAccReports();
+	void setYransactions();
 	~PettyCashier();
 };
 

62101671e1710a90a188307a857a4bb210b93f7e
pinsara <32256306+pinsara@users.noreply.github.com>
2017-10-05 13:32:32 +0530

    Update Administrator.cpp

--- pettycash/Administrator.cpp
+++ pettycash/Administrator.cpp
@@ -1,12 +1,29 @@
-#include "stdafx.h"
+#include "StdAfx.h"
 #include "Administrator.h"
 
 
-Administrator::Administrator()
+Administrator::Administrator(void)
 {
 }
+Administrator::Administrator(char pAdmin_ID[10],char pAcc_Pin[10],char pUser_Name[50],char pUser_Id[10],char pUser_Pin[10])
+{
+	Admin_ID[10] = pAdmin_ID[10];
+	Acc_Pin[10] = pAcc_Pin[10];
+	User_Name[50] = pUser_Name[50];
+	User_Id[10] = pUser_Id[10];
+	User_Pin[10] = pUser_Pin[10];
+}
+void Administrator::SetAccountPin(char pin[10])
+{
+	pin[10] = Acc_Pin[10];
+}
+void Administrator::AddUser()
+{
 
-
-Administrator::~Administrator()
+}
+void Administrator::RemoveUser()
+{
+}
+Administrator::~Administrator(void)
 {
 }
